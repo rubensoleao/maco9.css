38ed14c4bdef78541851e6c75557f241a7314b27
Rubens O Leao <rubensoleao@gmail.com>
2026-01-02 21:02:47 -0500

    Initial commit from zip

--- build.js
+++ build.js
@@ -1,38 +1,57 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const mkdirp = require('mkdirp');
-const path = require('path');
-const postcss = require('postcss');
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const postcss = require("postcss");
+const sass = require("sass");
 
-const { homepage, version } = require('./package.json');
+const { homepage, version } = require("./package.json");
 
-function buildCSS() {
-  const input =
-    `/*! platinum.css v${version} - ${homepage} */\n` +
-    fs.readFileSync('src/index.scss');
 
-  return postcss()
-    .use(require('postcss-inline-svg'))
-    .use(require('postcss-css-variables'))
-    .use(require('postcss-calc'))
-    .use(require('postcss-copy')({ dest: 'build', template: '[name].[ext]' }))
-    .use(require('cssnano'))
-    .process(input, {
-      syntax: require('postcss-scss'),
-      from: 'src/index.scss',
-      to: 'build/platinum.css',
-      map: { inline: false },
-    })
-    .then(result => {
-      mkdirp.sync('build');
-      fs.writeFileSync('build/platinum.css', result.css);
-      fs.writeFileSync('build/platinum.css.map', result.map.toString());
-    });
+// See if printing minified css
+const mode = process.argv[2] || "readable";
+const isMinified = mode === "min";
+
+async function buildCSS() {
+  const sassResult = sass.compile("src/index.scss", {
+    style: isMinified ? "compressed" : "expanded",
+    sourceMap: true,
+    loadPaths: ["node_modules", "src"],
+  }); 
+
+  const banner = `/*! os9.css v${version} - ${homepage} */\n`;
+  const cssWithBanner = banner + sassResult.css;
+
+  // 2) PostCSS on compiled CSS
+  const processor = postcss()
+    .use(require("postcss-inline-svg"))
+    .use(require("postcss-css-variables"))
+    .use(require("postcss-calc"))
+    .use(require("postcss-copy")({ dest: "build", template: "[name].[ext]" }))
+
+  if (isMinified) {
+    processor.use(require("cssnano"));
+  }
+
+  const result = await processor.process(cssWithBanner, {
+      from: "src/index.scss",
+      to: "build/os9.css",
+      map: {
+        inline: false,
+        prev: sassResult.sourceMap, // <--  chain sourcemaps
+      },
+  });
+
+  mkdirp.sync("build");
+  fs.writeFileSync("build/os9.css", result.css);
+  if (result.map) fs.writeFileSync("build/os9.css.map", result.map.toString());
 }
 
 function build() {
-  buildCSS().catch(err => console.log(err));
+  buildCSS().catch((err) => {
+    console.error(err);
+    process.exitCode = 1;
+  });
 }
-module.exports = build;
 
+module.exports = build;
 build();
